89ca94ed913b37fb7cfa07b440b98aa4401f117d
jokaidai <34964251+jokaidai@users.noreply.github.com>
2022-08-07 09:42:46 +0300

    level set up collision animations physics finished

---
+++ Assets/Graphics/Level/Tilesets/Background_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="Background_2" tilewidth="64" tileheight="64" tilecount="28" columns="7">
+ <image source="../LevelAssets/Background_2.png" width="496" height="272"/>
+</tileset>
---
+++ Assets/Graphics/Level/Tilesets/level_assets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="Assets" tilewidth="64" tileheight="64" tilecount="322" columns="23">
+ <image source="../LevelAssets/Assets.png" width="1472" height="896"/>
+</tileset>
